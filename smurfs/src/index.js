dfaaaf8cdfd8e6eab78c9e167a94796b166448b1
ebisLab <ebaiden@techie.com>
2019-08-16 12:33:53 -0400

    applied middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {reducer} from './reducers';
+import thunk from 'redux-thunk'
 import {Provider} from 'react-redux';
 
 
 
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store = {store }><App /></Provider>
