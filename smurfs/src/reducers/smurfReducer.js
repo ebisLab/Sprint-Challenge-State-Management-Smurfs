add894b74c1523b6aec2dd6070e4da82dc0bb43a
ebisLab <ebaiden@techie.com>
2019-08-16 11:46:22 -0400

    reducer installed

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    chars: [], 
+}
+
+export const reducer = (state = initialState, action) => {
+
+    switch(action.type){
+
+        default:
+            return state;
+    }
+}
