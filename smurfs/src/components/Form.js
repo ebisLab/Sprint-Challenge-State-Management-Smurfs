fad935a8a73506f36b451f219591b1124d7948b3
ebisLab <ebaiden@techie.com>
2020-02-21 11:22:14 -0500

    Form Created

---
+++ smurfs/src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const Form = () => {
+
+    return(
+        <div>
+
+<form>
+<input placeholder="Find smurf"/>
+<button>Submit</button>
+
+</form>
+        </div>
+    )
+}
+
+export default Form;
